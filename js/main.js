88c8c17bdc9f65c3405e4d397c91a887bbb25d55
Martin Naumann <martin@geekonaut.de>
2015-01-31 23:42:54 +0100

    The final sample code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var THREE = require('three'),
-    World = require('three-world');
+    World = require('three-world'),
+    OBJMTLLoader = require('./OBJMTLLoader');
 
-World.init({ambientLightColor: 0, camDistance: 100, renderCallback: render});
+World.init({camDistance: 100, renderCallback: render});
 
 var box = new THREE.Mesh(
   new THREE.BoxGeometry(10, 10, 10),
@@ -9,6 +10,17 @@ var box = new THREE.Mesh(
 );
 World.add(box);
 
+var loader = new OBJMTLLoader();
+var ship = undefined;
+
+loader.load("models/Feisar_Ship.obj",
+  "models/Feisar_Ship.mtl", function(obj3d) {
+  window.thing = obj3d;
+  obj3d.scale.set(0.25, 0.25, 0.25);
+  World.add(obj3d);
+  ship = obj3d;
+});
+/*
 var lightR = new THREE.PointLight(0xff0000, 1, 100);
 lightR.position.set(-10, 20, 10);
 var lightG = new THREE.PointLight(0x00ff00, 1, 100);
@@ -19,11 +31,13 @@ lightB.position.set(  0, -20, 10);
 World.add(lightR);
 World.add(lightG);
 World.add(lightB);
+*/
 
 function render() {
   box.rotation.y += -Math.PI / 1000;
   box.rotation.x +=  Math.PI / 1000;
 //  box.rotation.z += -Math.PI / 100;
+  if(ship) ship.rotation.y += Math.PI / 100;
 }
 
 World.start();
